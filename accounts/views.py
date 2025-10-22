97d669fa776795738f605a0d5547d5a87fb7a52b
Amirhosein Sebghati <amir.sebghati85@gmail.com>
2025-10-22 21:57:42 +0330

    Changes to be committed: 	modified:   accounts/templates/accounts/login.html 	modified:   accounts/urls.py 	modified:   accounts/views.py 	modified:   core/templates/core/base.html

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class Login(View):
+    def post(self, request):
+        pass
+    
+    def get(self, request):
+        return render(request, 'accounts/login.html', {})
