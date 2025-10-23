e1cff114bcc7491ccf2ed67f3447937495d5587d
Test <test@gmail.com>
2025-10-23 11:34:52 +0330

    modified:   accounts/models.py 	new file:   accounts/templates/accounts/edit-profile.html 	modified:   accounts/templates/accounts/register.html 	renamed:    accounts/templates/accounts/profile.html -> accounts/templates/accounts/view-profile.html 	modified:   accounts/urls.py 	modified:   accounts/views.py 	modified:   core/static/core/css/style.css 	modified:   orders/templates/orders/order_detail.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('register/', views.Register.as_view(), name='register'),
+    path('profile/', views.ViewProfile.as_view(), name='view-profile'),
+    path('edit-profile/', views.EditProfile.as_view(), name='edit-profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
 from django.views import View
 
+
 class Login(View):
     def post(self, request):
         pass
-    
+
     def get(self, request):
         return render(request, 'accounts/login.html', {})
+# -------------------------------------------------------------------------------------------------------
+
+
+class Logout(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        return render(request, 'accounts/logout.html', {})
+# -------------------------------------------------------------------------------------------------------
+
+
+class Register(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        return render(request, 'accounts/register.html', {})
+# -------------------------------------------------------------------------------------------------------
+
+
+class ViewProfile(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        return render(request, 'accounts/view-profile.html', {})
+# -------------------------------------------------------------------------------------------------------
+
+
+class EditProfile(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        return render(request, 'accounts/edit-profile.html', {})
+# -------------------------------------------------------------------------------------------------------
