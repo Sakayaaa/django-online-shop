2e5e4d17723f0b99893621ae9af4a0e2feca1917
Test <test@gmail.com>
2025-10-28 14:46:37 +0330

    modified:   accounts/validators.py

--- accounts/validators.py
+++ accounts/validators.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 
 def house_number_validator(value):
-    pattern = r'^\d{1,3}(/[\d]{1,2})?$'
+    pattern = r'^\d{1,3}(/\d{1})?$'
     if not re.match(pattern, value):
         raise ValidationError(
             'Invalid House Number. Valid Examples: 2, 24, 246, 24/1, ... (max length is 5 characters)'
