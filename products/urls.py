d95bcb2bab8104f300a4c552baf7a46d2c937524
Test <test@gmail.com>
2025-11-17 17:35:28 +0330

    new file:   products/templates/products/categories.html 	modified:   products/urls.py 	modified:   products/views.py

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('categories/', views.Categories.as_view(), name='categories'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Category
 
-
+class Categories(View):
+    def get(self, request):
+        root_categories = Category.objects.filter(parent__isnull=True)
+        return render(request, 'products/categories.html', {'root_categories':root_categories})
+    
+    def post(self, request):
+        pass
