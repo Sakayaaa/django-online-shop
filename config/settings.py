f88a10b7f68c17309ef9b260332e7d7d6d52a2b3
Test <test@gmail.com>
2025-11-18 16:45:20 +0330

    modified:   config/settings.py 	new file:   core/media/products/media/category/527-5274826_laptop-and-mobile-png-transparent-png.png 	new file:   core/media/products/media/category/adorable-brown-teddy-bear-plush-toy_632498-26315.avif 	new file:   core/media/products/media/category/elegant-modern-sofa-side-table-interior-design_191095-91270.avif 	new file:   core/media/products/media/category/golden-jewelry-chains-set_1284-11761.avif 	new file:   core/media/products/media/category/health-care-concept_93675-130417.avif 	new file:   core/media/products/media/category/images.jpg 	new file:   core/media/products/media/category/images_1.jpg 	new file:   core/media/products/media/category/images_2.jpg 	new file:   core/media/products/media/category/pngtree-gold-necklace-on-display-stand-png-image_11440173.png 	new file:   core/media/products/media/category/sweater-shirt-hoodie-isolated-png.png 	modified:   core/static/core/css/style.css 	renamed:    core/media/defaults/question_mark.jpg -> core/static/core/defaults/question_mark.jpg 	modified:   core/templates/core/navbar.html 	new file:   products/migrations/0006_alter_category_image_alter_product_image.py 	modified:   products/models.py 	modified:   products/templates/products/categories.html

--- config/settings.py
+++ config/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "core" / "static"]
+# STATIC_ROOT = BASE_DIR / "core" / "static"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "core" / "media"
