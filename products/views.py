d487e395d063ecaa83ad4a8124b58ddf43cbc963
Test <test@gmail.com>
2025-12-04 13:54:51 +0330

    modified:   products/templates/products/product_list.html 	modified:   products/views.py

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 from django.views import View
-from .models import Category, Product
+from .models import Category, Product, Brand
 
 
 class CategoryList(View):
@@ -30,19 +30,30 @@ class CategoryList(View):
 class ProductList(View):
     def get(self, request):
         category_slug = request.GET.get('category')
+        brand_slug = request.GET.get('brand')
+
         products = Product.objects.all()
         category = None
+        brand = None
+
         all_categories = Category.objects.all().order_by('tree_id', 'lft')
+        brands = Brand.objects.all()
 
         if category_slug:
             category = get_object_or_404(Category, slug=category_slug)
             descendants = category.get_descendants(include_self=True)
             products = products.filter(category__in=descendants)
 
+        if brand_slug:
+            brand = get_object_or_404(Brand, slug=brand_slug)
+            products = products.filter(brand=brand)
+
         context = {
             'products': products,
             'category': category,
+            'brand': brand,
             'all_categories': all_categories,
+            'brands': brands,
         }
 
         return render(request, 'products/product_list.html', context)
