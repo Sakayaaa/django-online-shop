bc069989fd3f675f585934c81358ebbe67fe5ab6
Amirhosein Sebghati <amir.sebghati85@gmail.com>
2026-02-20 21:34:38 +0330

    new file:   cart/migrations/0001_initial.py 	modified:   cart/models.py 	modified:   cart/urls.py

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from products.models import Product
+from django.conf import settings
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="cart")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["cart", "product"], name="unique_product_per_cart")
+        ]
--- cart/urls.py
+++ cart/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('show-cart/', views.ShowCart.as_view(), name='show-cart'),
-    path('add-to-cart/<>', views)
+    path('add-cart/<slug:slug>/', views.AddCart.as_view(), name='add-cart'),
+    path('remove-cart/<slug:slug>/', views.RemoveCart.as_view(), name='remove-cart'),
+    path('checkout/show-cart/', views.ShowCart.as_view(), name='show-cart'),
+    path('checkout/increase-quantity<slug:slug>/', views.IncreaseQuantity.as_view(), name='increase-quantity'),
+    path('checkout/decrease-quantity<slug:slug>/', views.DecreaseQuantity.as_view(), name='decrease-quantity'),
 ]
